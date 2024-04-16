988809c12e5fdea0a46d8294a8946ef38abb8be8
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2024-04-16 13:39:24 +0100

    recipe list added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Pepperoni Pizza with Cheese', 'https://www.iheartnaptime.net/wp-content/uploads/2023/04/Pepperoni-pizza.jpg'),
+    new Recipe('Burger', 'Hamburger', 'https://www.allrecipes.com/thmb/_OKqViGmlNaa9GV_c4cpwpwApGk=/0x512/filters:no_upscale():max_bytes(150000):strip_icc()/25473-the-perfect-basic-burger-DDMFS-4x3-56eaba3833fd4a26a82755bcd0be0c54.jpg')
+  ];
 
 }
